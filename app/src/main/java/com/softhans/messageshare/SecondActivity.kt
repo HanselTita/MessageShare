14a7bb4e49a2210041f40cb2a74f2ef3bfc501dd
Hansel Tita <hanseltita@gmail.com>
2020-03-23 23:52:56 +0700

    Explicit Intent

---
+++ app/src/main/java/com/softhans/messageshare/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.softhans.messageshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+    }
+}
